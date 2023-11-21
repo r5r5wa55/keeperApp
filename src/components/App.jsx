709662b36c6a68f5b7e7f47b59441cf3e9f424bf
werapol <r5r5wa55@hotmail.com>
2023-11-22 01:12:46 +0700

    Add-List

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+
+  
+  
+  const [fullText,setFullText]=useState([])
+
+  function addInput(title){
+  
+    setFullText((e)=>{
+      return [   ...e,
+        title]
+    })
+
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onChange={addInput}/>
+      {fullText.map(note=>{
+        return  <Note 
+  
+        title={note.title}
+        content={note.content} />
+      })}
+     
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+  const [contact,setNametext] = useState({
+    title:"",
+    content:""
+  });
+  
+  function showText(event){
+    const { name, value } = event.target;
+
+    
+    setNametext(prevValue => {
+      return {
+        ...prevValue,
+        [name]: value
+      };    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" placeholder="Title" onChange={showText} value={contact.title}/>
+        <textarea name="content" placeholder="Take a note..." rows="3"  onChange={showText} value={contact.content}/>
+        <button onClick={(event)=>{
+          event.preventDefault();
+          props.onChange(contact);
+          setNametext({
+            title:"",
+            content:""
+          });
+        
+            // addInput
+        }}>Add</button>
       </form>
     </div>
   );
